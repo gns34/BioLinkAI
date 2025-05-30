789e321963fee417fd119518e23c5daa226c9ee2
Gopi Nath <69713591+gns34@users.noreply.github.com>
2025-05-31 00:47:20 +0530

    add infinity app to settings and implement initial views

--- biolinkai/infinity/views.py
+++ biolinkai/infinity/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 def showinfinity(request):
+    template=loader.get_template('index.html')
     return HttpResponse("Hello world!")
