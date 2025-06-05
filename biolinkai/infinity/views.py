de68d7ce77ef8332c6b20ab2ee6ca4b5336acc20
Gopi Nath <69713591+gns34@users.noreply.github.com>
2025-06-05 18:33:38 +0530

    refactor showinfinity view to improve error handling and maintain code structure

--- biolinkai/infinity/views.py
+++ biolinkai/infinity/views.py
@@ -8,12 +8,15 @@ def showinfinity(request):
         firstname=request.POST.get('firstname')
         lastname=request.POST.get('lastname')
         phoneno=request.POST.get('phoneno')
-        Infinity.objects.create(
-            firstname=firstname,
-            lastname=lastname,
-            phoneno=phoneno
-        )
-
+        try:
+            Infinity.objects.create(
+                firstname=firstname,
+                lastname=lastname,
+                phoneno=phoneno
+            )
+        except:
+            return render(request, 'index.html',{'error':'Exception'})
+        
         return render(request, 'index.html',{'success':'Information Saved'})
     else:
         return render(request, 'index.html',{'error':'Some Error Occured'})
